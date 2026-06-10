e7b26d1b189b3fce77ac5fb7ca466074f1ff93a6
fazarhungkul82-cyber <fazarhungkul82@gmail.com>
2026-06-10 10:43:43 +0700

    Create ai.py

---
+++ modules/ai.py
@@ -0,0 +1,38 @@
+from openai import OpenAI
+import streamlit as st
+
+client = OpenAI(
+    api_key=st.secrets["OPENAI_API_KEY"]
+)
+
+def ask_ai(context, question):
+
+    with open(
+        "prompts/system_prompt.txt",
+        "r",
+        encoding="utf-8"
+    ) as f:
+        system_prompt = f.read()
+
+    response = client.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=[
+            {
+                "role":"system",
+                "content":system_prompt
+            },
+            {
+                "role":"user",
+                "content":f"""
+Context:
+{context}
+
+Question:
+{question}
+"""
+            }
+        ],
+        temperature=0.2
+    )
+
+    return response.choices[0].message.content
